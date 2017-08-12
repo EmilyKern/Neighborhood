7394cc3f640e1d75a7bb2837f5d232003fcb5f1b
EmilyKern <kernec@ldschurch.org>
2017-08-12 12:51:16 -0600

    Add files via upload

--- hoodknockout.js
+++ hoodknockout.js
@@ -14,9 +14,12 @@ function initMap() {
 	//styles: styles,
 	mapTypeControl: false
 });	
+
+	createMarkersForPlaces(places);
 }
 
-/*// Create a searchbox in order to execute a place search
+/*
+	// Create a searchbox in order to execute a place search
 	var searchBox = new google.maps.places.SearchBox(
 		document.getElementById('places-search'));
 	// Limit the searchbox to within the bounds of the map.
@@ -65,8 +68,19 @@ function initMap() {
 
     document.getElementById('hide-listings').addEventListener('click', function() {
           hideMarkers(markers);
-    });*/
+    });
 
+    // Listen for the event fired when the user selects a prediction from the
+    // picklist and retrieve more details for that place.
+    searchBox.addListener('places_changed', function() {
+        searchBoxPlaces(this);
+    });
+
+    // Listen for the event fired when the user selects a prediction and clicks
+    // "go" more details for that place.
+    document.getElementById('go-places').addEventListener('click', textSearchPlaces);
+
+*/
 
 // Model
 
@@ -80,6 +94,157 @@ function initMap() {
    {title: 'Conference Center', location: {lat: 40.772623, lng: -111.892351}}
  ];
 
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          // Clear the infowindow content to give the streetview time to load.
+          infowindow.setContent('');
+          infowindow.marker = marker;
+          	// Make sure the marker property is cleared if the infowindow is closed.
+          	infowindow.addListener('closeclick', function() {
+            	infowindow.marker = null;
+          	});
+		}
+}
+
+// This function will loop through the markers array and display them all.
+function showListings() {
+	var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+    	markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+}
+
+
+// This function will loop through the listings and hide them all.
+function hideMarkers(markers) {
+	for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+}
+
+// This function fires when the user selects a searchbox picklist item.
+// It will do a nearby search using the selected query string or place.
+function searchBoxPlaces(searchBox) {
+    hideMarkers(placeMarkers);
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+        window.alert('We did not find any places matching that search!');
+    } else {
+        // For each place, get the icon, name and location.
+        createMarkersForPlaces(places);
+    }
+}
+
+// This function firest when the user select "go" on the places search.
+// It will do a nearby search using the entered query string or place.
+function textSearchPlaces() {
+    var bounds = map.getBounds();
+    hideMarkers(placeMarkers);
+    var placesService = new google.maps.places.PlacesService(map);
+    placesService.textSearch({
+        query: document.getElementById('places-search').value,
+        bounds: bounds
+    }, function(results, status) {
+        	if (status === google.maps.places.PlacesServiceStatus.OK) {
+            createMarkersForPlaces(results);
+        	}
+      	});
+}
+
+// This function creates markers for each place found in either places search.
+function createMarkersForPlaces(places) {
+  	var bounds = new google.maps.LatLngBounds();
+  	for (var i = 0; i < places.length; i++) {
+    	var place = places[i];
+    	var icon = {
+      		url: place.icon,
+      		size: new google.maps.Size(35, 35),
+      		origin: new google.maps.Point(0, 0),
+      		anchor: new google.maps.Point(15, 34),
+      		scaledSize: new google.maps.Size(25, 25)
+    	};
+		// Create a marker for each place.
+		var marker = new google.maps.Marker({
+  			map: map,
+  			icon: icon,
+  			title: place.name,
+  			position: place.geometry.location,
+  			id: place.place_id
+		});
+        // Create a single infowindow to be used with the place details information
+        // so that only one is open at once.
+        var placeInfoWindow = new google.maps.InfoWindow();
+        // If a marker is clicked, do a place details search on it in the next function.
+        marker.addListener('click', function() {
+        	if (placeInfoWindow.marker == this) {
+              console.log("This infowindow already is on this marker!");
+            } else {
+              getPlacesDetails(this, placeInfoWindow);
+            }
+        });
+        placeMarkers.push(marker);
+        if (place.geometry.viewport) {
+        	// Only geocodes have viewport.
+        	bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
+    }
+    map.fitBounds(bounds);
+}
+
+// This is the PLACE DETAILS search - it's the most detailed so it's only
+// executed when a marker is selected, indicating the user wants more
+// details about that place.
+function getPlacesDetails(marker, infowindow) {
+	var service = new google.maps.places.PlacesService(map);
+  	service.getDetails({
+    	placeId: marker.id
+  	}, function(place, status) {
+     	if (status === google.maps.places.PlacesServiceStatus.OK) {
+        	// Set the marker property on this infowindow so it isn't created again.
+          	infowindow.marker = marker;
+          	var innerHTML = '<div>';
+          	if (place.name) {
+            	innerHTML += '<strong>' + place.name + '</strong>';
+          	}
+          	if (place.formatted_address) {
+            	innerHTML += '<br>' + place.formatted_address;
+          	}
+          	if (place.formatted_phone_number) {
+            	innerHTML += '<br>' + place.formatted_phone_number;
+          	}
+          	if (place.opening_hours) {
+            	innerHTML += '<br><br><strong>Hours:</strong><br>' +
+                place.opening_hours.weekday_text[0] + '<br>' +
+                place.opening_hours.weekday_text[1] + '<br>' +
+                place.opening_hours.weekday_text[2] + '<br>' +
+                place.opening_hours.weekday_text[3] + '<br>' +
+                place.opening_hours.weekday_text[4] + '<br>' +
+                place.opening_hours.weekday_text[5] + '<br>' +
+                place.opening_hours.weekday_text[6];
+          	}
+          	if (place.photos) {
+            	innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+                	{maxHeight: 100, maxWidth: 200}) + '">';
+          		}
+        	innerHTML += '</div>';
+          	infowindow.setContent(innerHTML);
+          	infowindow.open(map, marker);
+          	// Make sure the marker property is cleared if the infowindow is closed.
+          	infowindow.addListener('closeclick', function() {
+            	infowindow.marker = null;
+          	});
+        }
+    });
+}
 
 //  View Model
 
