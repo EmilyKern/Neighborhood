630ab40a88bff5a4476d3be3016a856c670258bd
EmilyKern <kernec@ldschurch.org>
2017-09-06 12:53:02 -0600

    Add files via upload

--- hoodknockout2.js
+++ hoodknockout2.js
@@ -1,7 +1,8 @@
 var map;
+var infoWindow;
 var markers = []; // Creates a new blank array for all the listing markers.
 
-// Creates placemarkers array to use in multiple 
+// Creates placemarkers array to use in multiple
 // functions to have control over the number of places that show.
 var placeMarkers = [];
 
@@ -12,57 +13,57 @@ var vm;
 // These are the tourist spots listings that will be shown to the user.
 // Normally we'd have these in a database instead.
 var locations = [{
-    title: 'Salt Lake Temple',
-    location: {
-      lat: 40.770448,
-      lng: -111.891908
+        title: 'Salt Lake Temple',
+        location: {
+            lat: 40.770448,
+            lng: -111.891908
+        }
+    },
+    {
+        title: 'Temple Square',
+        location: {
+            lat: 40.769351,
+            lng: -111.894539
+        }
+    },
+    {
+        title: 'Church History Museum',
+        location: {
+            lat: 40.770874,
+            lng: -111.89442
+        }
+    },
+    {
+        title: 'Church History Library',
+        location: {
+            lat: 40.77208,
+            lng: -111.890406
+        }
+    },
+    {
+        title: 'Conference Center',
+        location: {
+            lat: 40.772623,
+            lng: -111.892351
+        }
     }
-  },
-  {
-    title: 'Temple Square',
-    location: {
-      lat: 40.769351,
-      lng: -111.894539
-    }
-  },
-  {
-    title: 'Church History Museum',
-    location: {
-      lat: 40.770874,
-      lng: -111.89442
-    }
-  },
-  {
-    title: 'Church History Library',
-    location: {
-      lat: 40.77208,
-      lng: -111.890406
-    }
-  },
-  {
-    title: 'Conference Center',
-    location: {
-      lat: 40.772623,
-      lng: -111.892351
-    }
-  }
 ];
 
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 40.7697,
-      lng: -111.894402
-    },
-    zoom: 16,
-    //styles: styles,
-    mapTypeControl: false
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.7697,
+            lng: -111.894402
+        },
+        zoom: 16,
+        //styles: styles,
+        mapTypeControl: false
+    });
 
-  vm = new ViewModel();
+    vm = new ViewModel();
 
-  ko.applyBindings(vm);
+    ko.applyBindings(vm);
 };
 
 
@@ -70,49 +71,51 @@ function initMap() {
 
 var Location = function(data) {
 
-  this.title = data.title;
+    // store a reference to this so if it changes, we can still
+    // access every property of the instance
+    var self = this;
 
-  this.marker = new google.maps.Marker({
-    map: map,
-    position: data.location,
-    title: data.title,
-    animation: google.maps.Animation.DROP  
-  });
+    self.title = data.title;
 
-  // from: http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
-  
-  /* now inside your initialise function */
-  var infowindow = new google.maps.InfoWindow({
-  content: "holding..."
-  });
+    self.marker = new google.maps.Marker({
+        map: map,
+        position: data.location,
+        title: data.title,
+        animation: google.maps.Animation.DROP
+    });
 
-  for (var i = 0; i < markers.length; i++) {
-  var marker = markers[i];
-  
-  google.maps.event.addListener(marker, 'click', function () {
-  // where I have added .html to the marker object.
-  //infowindow.setContent(this.html);
-  //infowindow.open(map, this);
-  infowindow.open(map, markers[i]);
-  // Put this in so that this small window closes if the user clicks
-  // the marker, when the big infowindow opens
-    markers[i].infowindow = infowindow;
-    google.maps.event.addListener(markers[i], 'click', function() {
-        this.infowindow.close();
-  });
-  };
+    self.wikiText = "loading";
 
+    // create an event listener for the newly created marker.
+    // the function will run when the marker is clicked
+    google.maps.event.addListener(self.marker, 'click', function() {
+      // update the contents of the infowindow object when the marker is clicked
+      infoWindow.setContent(self.wikiText);
+      // open the infowinow object at the position of the marker
+      infoWindow.open(map, self.marker);
+    });
+
+    // TODO:
+    // 1) call the wikipedia API using $.ajax()
+    // 2) WHEN it returns (inside the success:) update self.wikiText with something you retreived from wikipedia
+    // This means that when the marker is clicked, it will open with info you received from wikipedia (because it opens with self.wikiText)
 };
+
 
 var ViewModel = function() {
-  //this.locations = ko.observableArray(locations);
+    //this.locations = ko.observableArray(locations);
 
-  this.locations = ko.observableArray();
+    this.locations = ko.observableArray();
 
-  for (var i = 0; i < locations.length; i++) {
-    this.locations.push(new Location(locations[i])); 
-  }
+    // create a new infowindow object and assign it to the global infoWindow variable
+    infoWindow = new google.maps.InfoWindow({
+        content: "holding..."
+    });
+
+    for (var i = 0; i < locations.length; i++) {
+        this.locations.push(new Location(locations[i]));
+    }
 };
 
 
-//#sourceUrl=hoodknockout2.js
+            //#sourceUrl=hoodknockout2.js
