b70b40433077d0c5cd345cd3851c792647f96de0
EmilyKern <kernec@ldschurch.org>
2017-09-11 23:42:14 -0600

    Add files via upload

--- hoodknockout2.js
+++ hoodknockout2.js
@@ -1,8 +1,8 @@
 var map;
-var infoWindow;
+var infowindow;
 var markers = []; // Creates a new blank array for all the listing markers.
 
-// Creates placemarkers array to use in multiple
+// Creates placemarkers array to use in multiple 
 // functions to have control over the number of places that show.
 var placeMarkers = [];
 
@@ -12,138 +12,113 @@ var vm;
 
 // These are the tourist spots listings that will be shown to the user.
 // Normally we'd have these in a database instead.
-// locations is an array
 var locations = [{
-        title: 'Salt Lake Temple',
-        location: {
-            lat: 40.770448,
-            lng: -111.891908
-        }
-    },
-    {
-        title: 'Temple Square',
-        location: {
-            lat: 40.769351,
-            lng: -111.894539
-        }
-    },
-    {
-        title: 'Church History Museum',
-        location: {
-            lat: 40.770874,
-            lng: -111.89442
-        }
-    },
-    {
-        title: 'Church History Library',
-        location: {
-            lat: 40.77208,
-            lng: -111.890406
-        }
-    },
-    {
-        title: 'Conference Center',
-        location: {
-            lat: 40.772623,
-            lng: -111.892351
-        }
+    title: 'Salt Lake Temple',
+    location: {
+      lat: 40.770448,
+      lng: -111.891908
     }
+  },
+  {
+    title: 'Temple Square',
+    location: {
+      lat: 40.769351,
+      lng: -111.894539
+    }
+  },
+  {
+    title: 'Church History Museum',
+    location: {
+      lat: 40.770874,
+      lng: -111.89442
+    }
+  },
+  {
+    title: 'Church History Library',
+    location: {
+      lat: 40.77208,
+      lng: -111.890406
+    }
+  },
+  {
+    title: 'Conference Center',
+    location: {
+      lat: 40.772623,
+      lng: -111.892351
+    }
+  }
 ];
 
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 40.7697,
-            lng: -111.894402
-        },
-        zoom: 16,
-        //styles: styles,
-        mapTypeControl: false
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 40.7697,
+      lng: -111.894402
+    },
+    zoom: 16,
+    //styles: styles,
+    mapTypeControl: false
+  });
 
-    vm = new ViewModel();
+  infowindow = new google.maps.InfoWindow({
+    content: "holding..."
+  });
 
-    ko.applyBindings(vm);
+  vm = new ViewModel();
+
+  ko.applyBindings(vm);
 };
 
 
-
+//  View Model
 
 var Location = function(data) {
 
-    // store a reference to this so if it changes, we can still
-    // access every property of the instance
-    var self = this;
+  this.title = data.title;
 
-    self.title = data.title;
+  this.marker = new google.maps.Marker({
+    map: map,
+    position: data.location,
+    title: data.title,
+    animation: google.maps.Animation.DROP  
+  });
 
-    self.marker = new google.maps.Marker({
-        map: map,
-        position: data.location,
-        title: data.title,
-        animation: google.maps.Animation.DROP
+  // from: http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+  
+  /* now inside your initialise function */
+ 
+    google.maps.event.addListener(this.marker, 'click', function () {
+    // where I have added .html to the marker object.
+      infowindow.setContent(this.title);
+      infowindow.open(map, this);
     });
-
-    self.wikiText = "loading";
-
-    // create an event listener for the newly created marker.
-    // the function will run when the marker is clicked
-    google.maps.event.addListener(self.marker, 'click', function() {
-      // update the contents of the infowindow object when the marker is clicked
-      
-      infoWindow.setContent(self.wikiText);
-      // open the infowinow object at the position of the marker
-      infoWindow.open(map, self.marker);
-    });
-
-    // TODO:
-    // 1) call the wikipedia API using $.ajax()
-    
-    /*// Wikipedia code AJAX request
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.title + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function() {
-        ViewModel.wikiError("failed to get Wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiURL,
-        dataType: "jsonp",
-        success: function(response){
-            var articleList = response[1];
-            articleList.forEach(function(articleStr){
-                var url = 'http://wikipedia.org/wiki/' + articleStr;
-                viewModel.wikiArray.push({url: url, article: articleStr});
-                return(response);
-            }
-        }
-    })
-    // 2) WHEN it returns (inside the success:) update self.wikiText with something you retreived from wikipedia
-    // This means that when the marker is clicked, it will open with info you received from wikipedia (because it opens with self.wikiText)
-
-    self.handleClick = function(){
-        //.....
-    }
-    self.wikiText = url; */
 
 };
-
-// View Model
 
 var ViewModel = function() {
-    //this.locations = ko.observableArray(locations);
+  //this.locations = ko.observableArray(locations);
 
-    this.locations = ko.observableArray();
+  this.locations = ko.observableArray();
 
-    // create a new infowindow object and assign it to the global infoWindow variable
-    infoWindow = new google.maps.InfoWindow({
-        content: "holding..."
-    });
+  for (var i = 0; i < locations.length; i++) {
+    this.locations.push(new Location(locations[i])); 
+  }
 
-    for (var i = 0; i < locations.length; i++) {
-        this.locations.push(new Location(locations[i]));
-    }
+  // add the list view items' click event handler method there
+  // use the 'current item' aka first parameter to activate the corresponding map marker
+  // http://knockoutjs.com/documentation/click-binding.html#note-1-passing-a-current-item-as-a-parameter-to-your-handler-function
+  function MyViewModel() {
+    var self = this;
+      self.locations = ko.observableArray();
+ 
+      // The current item will be passed as the first parameter, so we know which place to remove
+      self.removePlace = function(place) {
+        self.places.remove(place)
+        }
+  }
+  ko.applyBindings(new MyViewModel());
 };
 
 
-            //#sourceUrl=hoodknockout2.js
+//#sourceUrl=hoodknockout2.js
